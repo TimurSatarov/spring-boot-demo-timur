fe3e108fd3bded7c5f23bdbc0a9dad294a018176
TimurSatarov <timursatarov3@gmail.com>
2025-10-23 07:30:35 +0200

    update files

---
+++ demo/src/test/java/spring_boot_demo_timur/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package spring_boot_demo_timur.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
